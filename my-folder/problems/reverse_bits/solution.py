0a2a2ea3dab553c0f219389a1edbcb5d61ebd434
Azhan <47063586+azhan619@users.noreply.github.com>
2022-07-03 12:34:36 +0000

    Sync LeetCode submission - Reverse Bits (python3)

---
+++ my-folder/problems/reverse_bits/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        output = 0
+        
+        for i in range(32):
+            
+            bit = (n >> i) & 1
+            
+            output = output | (bit << (31-i))
+            
+        return output     
+            
+        
